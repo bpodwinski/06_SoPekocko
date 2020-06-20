1d121ccb6f7350b4d9dcab8ec9d686dc150d208b
Benoit Podwinski <me@benoitpodwinski.com>
2020-06-20 18:52:46 +0200

    Use Proxy for fixing CORS issues https://www.techiediaries.com/fix-cors-with-angular-cli-proxy-configuration/

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,30 +1,33 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
-import { Router } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject } from "rxjs";
+import { Router } from "@angular/router";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthService {
-
   isAuth$ = new BehaviorSubject<boolean>(false);
   private authToken: string;
   private userId: string;
 
-  constructor(private http: HttpClient,
-              private router: Router) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   createUser(email: string, password: string) {
     return new Promise((resolve, reject) => {
-      this.http.post('http://localhost:3000/api/auth/signup', {email: email, password: password}).subscribe(
-        (response: { message: string }) => {
-          resolve(response);
-        },
-        (error) => {
-          reject(error);
-        }
-      );
+      this.http
+        .post("/api/auth/signup", {
+          email: email,
+          password: password,
+        })
+        .subscribe(
+          (response: { message: string }) => {
+            resolve(response);
+          },
+          (error) => {
+            reject(error);
+          }
+        );
     });
   }
 
@@ -38,17 +41,22 @@ export class AuthService {
 
   loginUser(email: string, password) {
     return new Promise((resolve, reject) => {
-      this.http.post('http://localhost:3000/api/auth/login', {email: email, password: password}).subscribe(
-        (response: {userId: string, token: string}) => {
-          this.userId = response.userId;
-          this.authToken = response.token;
-          this.isAuth$.next(true);
-          resolve();
-        },
-        (error) => {
-          reject(error);
-        }
-      );
+      this.http
+        .post("/api/auth/login", {
+          email: email,
+          password: password,
+        })
+        .subscribe(
+          (response: { userId: string; token: string }) => {
+            this.userId = response.userId;
+            this.authToken = response.token;
+            this.isAuth$.next(true);
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
     });
   }
 
@@ -56,7 +64,6 @@ export class AuthService {
     this.authToken = null;
     this.userId = null;
     this.isAuth$.next(false);
-    this.router.navigate(['login']);
+    this.router.navigate(["login"]);
   }
-
 }
