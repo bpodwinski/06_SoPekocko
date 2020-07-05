3cccc7bcc4a201a7a9836264d8b5fb2514358520
Benoit Podwinski <me@benoitpodwinski.com>
2020-07-05 12:50:25 +0200

    Adding Like and Dislike API

--- backend/src/controllers/sauces.controller.ts
+++ backend/src/controllers/sauces.controller.ts
@@ -1,24 +1,9 @@
-import * as express from "express";
 import { Request, Response, NextFunction } from "express";
 import * as fs from "fs";
 import * as Server from "../server";
 import Sauces from "../models/sauces.model";
 
 export default class SaucesController {
-  public router = express.Router();
-
-  constructor() {
-    this.initRoutes();
-  }
-
-  public initRoutes() {
-    this.router.get("/sauces", this.getAllSauce);
-    this.router.get("/sauces/:id", this.getOneSauce);
-    this.router.post("/sauces", this.createSauce);
-    this.router.put("/sauces/:id", this.modifySauce);
-    this.router.delete("/sauces/:id", this.deleteSauce);
-  }
-
   // Get all sauces
   public async getAllSauce(req: Request, res: Response, next: NextFunction) {
     try {
@@ -97,6 +82,65 @@ export default class SaucesController {
       res.status(200).json({ message: sauce });
     } catch (error) {
       next(error);
+    }
+  }
+  // Like and Dislike
+  public likeOrDislike(req: Request, res: Response, next: NextFunction) {
+    // User like = 1
+    if (req.body.like === 1) {
+      Sauces.updateOne(
+        { _id: req.params.id },
+        {
+          $inc: { likes: req.body.like },
+          $push: { usersLiked: req.body.userId },
+        }
+      )
+        .then((sauce) => res.status(200).json({ message: sauce }))
+        .catch((error) => next(error));
+
+      // User dislike = -1
+    } else if (req.body.like === -1) {
+      Sauces.updateOne(
+        { _id: req.params.id },
+        {
+          $inc: { dislikes: req.body.like * -1 },
+          $push: { usersDisliked: req.body.userId },
+        }
+      )
+        .then((sauce) => res.status(200).json({ message: sauce }))
+        .catch((error) => next(error));
+
+      // User like = 0
+    } else {
+      Sauces.findOne({ _id: req.params.id })
+        .then((sauce) => {
+          if (!sauce) {
+            throw next(new Error("Not found"));
+          }
+          if (sauce.usersLiked.includes(req.body.userId)) {
+            Sauces.updateOne(
+              { _id: req.params.id },
+              { $pull: { usersLiked: req.body.userId }, $inc: { likes: -1 } }
+            )
+              .then((sauce) => {
+                res.status(200).json({ message: sauce });
+              })
+              .catch((error) => next(error));
+          } else if (sauce.usersDisliked.includes(req.body.userId)) {
+            Sauces.updateOne(
+              { _id: req.params.id },
+              {
+                $pull: { usersDisliked: req.body.userId },
+                $inc: { dislikes: -1 },
+              }
+            )
+              .then((sauce) => {
+                res.status(200).json({ message: sauce });
+              })
+              .catch((error) => next(error));
+          }
+        })
+        .catch((error) => next(error));
     }
   }
 }
